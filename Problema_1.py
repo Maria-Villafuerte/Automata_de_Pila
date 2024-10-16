bc15cabd005a6a4bcd9a837dc6e5f508f710e580
Maria-Villafuerte <villafuerte.mariajose046@gmail.com>
2024-10-16 17:19:37 -0600

    fix: problema 1 cadenas 0011

--- Problema_1.py
+++ Problema_1.py
@@ -4,46 +4,50 @@ rechazada = False
 
 cadena = input("Ingrese la cadena a comprobar: ") + 'F'
 
-for simbolo in cadena:  
+for simbolo in cadena:
     if rechazada:
         break
 
-    enestack = stack[-1]  # Ver qué hay en la cima de la pila
+    enestack = stack[-1]  # Ver la cima de la pila
 
     if estado_actual == 'q0':
         if simbolo == '0':
-            if enestack == 'Z' or enestack == 'X':
-                stack.extend(['X', 'X'])
-                if enestack == 'Z':
-                    stack.pop()  # Eliminar Z tras insertar
-                else:
-                    stack.pop()  # Si era X, igual se elimina una X
+            if enestack == 'Z':
+                stack.append('X')
+                stack.append('X')
+                stack.append('Z')
+                stack.pop()  # Quitamos la 'Z'
+            elif enestack == 'X':
+                stack.append('X')
+                stack.append('X')
+                stack.pop()  # Quitamos la 'X'
             else:
                 rechazada = True
-
-        elif simbolo == '1' and enestack == 'X':
-            estado_actual = 'q1'
-            stack.pop()  # Consumir una X
-        else:
-            rechazada = True
-
-    elif estado_actual == 'q1':
-        if simbolo == '0':
-            rechazada = True  # No se permiten 0's en q1
 
         elif simbolo == '1':
             if enestack == 'X':
-                stack.pop()  # Consumir una X
+                estado_actual = 'q1'
+                stack.pop()  # Quitamos la 'X'
             else:
                 rechazada = True
 
-        elif simbolo == 'F' and enestack == 'Z':
-            estado_actual = 'q2'
-            stack.pop()  # Finalizar aceptando
-            break
+    elif estado_actual == 'q1':
+        if simbolo == '0':
+            rechazada = True  # No se permiten '0' en q1
 
-        else:
-            rechazada = True
+        elif simbolo == '1':
+            if enestack == 'X':
+                stack.pop()  # Quitamos la 'X'
+            else:
+                rechazada = True
+
+        elif simbolo == 'F':
+            if enestack == 'Z':
+                estado_actual = 'q2'
+                stack.pop()  # Quitamos la 'Z'
+                break
+            else:
+                rechazada = True
 
 # Resultado final
 if not rechazada and estado_actual == 'q2':
